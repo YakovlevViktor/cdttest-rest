bde872b0a5d7250fdd4cb6c9ac219623d2159d0d
vyakovlev <yakovlevvik51@gmail.com>
2018-04-26 10:06:07 +0300

    couple of methods implemented

--- rest.py
+++ rest.py
@@ -1,10 +1,11 @@
-import sys
 import json
+import sys
 from datetime import datetime
 
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
+connect_str = ''
 if len(sys.argv) != 2:
     print("Usage: python %s %s" % (sys.argv[0], "db_connect_string"))
 else:
@@ -41,14 +42,44 @@ class Task(db.Model):
     batch__oid = db.Column('batch__oid', db.Integer, db.ForeignKey('batch.id'), nullable=False)
     batch = db.relationship('Batch', backref=db.backref('tasks', lazy=True))
 
+    def toJSON(self):
+        res = {
+            "id": self.id,
+            "url": self.url,
+            "status": self.status,
+            "batch__oid": self.batch__oid
+        }
+        return res
 
-@app.route('/cdtest/api/v1.0/batch/<int:batch_id>', methods=['GET'])
-def get_batch(batch_id):
+
+@app.route('/cdtest/api/v1.0/batch/id/<int:batch_id>', methods=['GET'])
+def get_batch_by_id(batch_id):
     res = db.session.query(Batch).get(batch_id)
     res = res.toJSON()
     return json.dumps(res)
-    # res = json.dumps(res)
-    # return res
+
+
+@app.route('/cdtest/api/v1.0/batch/file/<string:file_in>', methods=['GET'])
+def get_batch_by_file(file_in):
+    res = db.session.query(Batch).filter(Batch.file_in.__eq__(file_in)).all()
+    json_data = {"batch": []}
+    for res_item in res:
+        json_item = res_item.toJSON()
+        print(json_item)
+        json_data["batch"].append(json_item)
+    return json.dumps(json_data)
+
+@app.route('/cdtest/api/v1.0/batch/date', methods=['GET'])
+def get_batch_by_date():
+    date_from = request.args.get('from')
+    date_to = request.args.get('to')
+    res = db.session.query(Batch).filter(Batch.datetime.between(date_from, date_to))
+    json_data = {"batch": []}
+    for res_item in res:
+        json_item = res_item.toJSON()
+        print(json_item)
+        json_data["batch"].append(json_item)
+    return json.dumps(json_data)
 
 
 app.run()
